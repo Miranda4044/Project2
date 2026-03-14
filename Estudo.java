5a5c6cc07f7da4698ede0a215c71a5f1e9c0c074
Miranda4044 <zac9900.jp@gmail.com>
2026-03-14 20:59:54 -0300

    Primeiros projetos
    Inicio do curso de Java

---
+++ Estudo.java
@@ -0,0 +1,7 @@
+public class Estudo {
+    public static void main(String[] args){
+        System.out.println("JVM (Java virtual machine)- é a Maquina virtual do Java responsável pro executar o bytecode.");
+        System.out.println("JRE (Java Runtime Eviroment)- ambiente de execução do java que fornece as bibliotecas padrões do java para a jdk compilar o seu código e para a JVM executar seu programa");
+
+    }
+}
---
+++ ScreenMatch.java
@@ -0,0 +1,29 @@
+public class ScreenMatch {
+    public static void main(String[] args) {
+    System.out.println("Esse é o Screen Match"); // faz surgir o titulo do app
+    System.out.println("Filme: Top Gun: Maverick"); // faz surgir o nome do filme
+
+int anoDeLancamento = 2022; // Ano de lançamento
+        System.out.println("Ano De Lancamento: " + anoDeLancamento); // faz surgir o ano de lançamento
+        boolean incluidoNoPlano = true; // para saber se o plano cobre o filme
+        double notaDoFilme = 8.1; // nota de avaliação
+        System.out.println("Notas de Avaliação; " + notaDoFilme); // faz surgir a nota do filme
+
+        //Notas de avalição de publico recente
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+
+
+
+
+}
+}
